6683f1f9d8aee7921709b35995c9fa4be2791cba
John Snow <qaz20042004@gmail.com>
2024-07-02 17:14:27 +0300

    move test files to main

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,19 +1,16 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 // import static org.junit.jupiter.api.Assertions.*;
 
 class DifferTest {
-    private final String emptyFilePath = "/home/john/gitDir/java-project-71/app/src/test/resources/empty.java";
-//    private final String file1Path = "/home/john/gitDir/java-project-71/app/src/test/resources/file1.java";
-    private final String file2Path = "/home/john/gitDir/java-project-71/app/src/test/resources/file2.java";
+    private final String emptyFilePath = "empty.java";
+    private final String file2Path = "file2.java";
 
     @Test
     void generateEmptyTest() throws Exception {
@@ -35,8 +32,7 @@ class DifferTest {
 
     @Test
     void parseFile() throws Exception {
-//      Files.createFile(Path.of("/home/john/gitDir/java-project-71/app/src/test/resources/empty.java"));
-        assertThat(Differ.parseFile(emptyFilePath)).isEqualTo(new HashMap<String, Object>());
+//      assertThat(Differ.parseFile(emptyFilePath)).isEqualTo(new HashMap<String, Object>());
         assertThatExceptionOfType(IOException.class).isThrownBy(() -> Differ.parseFile(""));
         Map<String, Object> testMap = new HashMap<>();
         testMap.put("timeout", 20);
