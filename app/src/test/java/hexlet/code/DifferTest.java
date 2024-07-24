e3428730e28fd3955d9d62913cd22f5ff81ebc4f
John Snow <qaz20042004@gmail.com>
2024-07-24 13:48:13 +0300

    test mod

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -9,21 +9,20 @@ class DifferTest {
     private static final String RES_FOLDER = System.getProperty("user.dir") + "/src/test/resources/";
 
     @Test
-    void stylishFormatTest() throws Exception {
-        Files.isDirectory(Path.of(RES_FOLDER + "stylish.tst"));
-        assertThat(Differ.generate(RES_FOLDER + "data.java", RES_FOLDER + "data2.java", "stylish")).
-                isEqualTo(Files.readString(Path.of(RES_FOLDER + "stylish.tst")));
-        assertThat(Differ.generate(RES_FOLDER + "data.yaml", RES_FOLDER + "data2.yaml", "stylish")).
-                isEqualTo(Files.readString(Path.of(RES_FOLDER + "stylish.tst")));
-    }
-
-    @Test
     void plainFormatTest() throws Exception {
         assertThat(Differ.generate(RES_FOLDER + "data.java", RES_FOLDER + "data2.java", "plain")).
                 isEqualTo(Files.readString(Path.of(RES_FOLDER + "plain.tst")));
         assertThat(Differ.generate(RES_FOLDER + "data.yaml", RES_FOLDER + "data2.yaml", "plain")).
                 isEqualTo(Files.readString(Path.of(RES_FOLDER + "plain.tst")));
     }
+    @Test
+    void stylishFormatTest() throws Exception {
+        assertThat(Differ.generate(RES_FOLDER + "data.java", RES_FOLDER + "data2.java", "stylish")).
+                isEqualTo(Files.readString(Path.of(RES_FOLDER + "stylish.tst")));
+        assertThat(Differ.generate(RES_FOLDER + "data.yaml", RES_FOLDER + "data2.yaml", "stylish")).
+                isEqualTo(Files.readString(Path.of(RES_FOLDER + "stylish.tst")));
+    }
+
 /*    @Test
     void jsonFormatTest() throws Exception {
         Differ.generate(RES_FOLDER + "data.java", RES_FOLDER + "data2.java", "json");
