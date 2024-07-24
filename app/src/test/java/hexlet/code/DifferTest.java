d289fb33199523432da5d54c54e75a038cc20b82
John Snow <qaz20042004@gmail.com>
2024-07-24 13:39:28 +0300

    test mod

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -10,6 +10,7 @@ class DifferTest {
 
     @Test
     void stylishFormatTest() throws Exception {
+        Files.isDirectory(Path.of(RES_FOLDER + "stylish.tst"));
         assertThat(Differ.generate(RES_FOLDER + "data.java", RES_FOLDER + "data2.java", "stylish")).
                 isEqualTo(Files.readString(Path.of(RES_FOLDER + "stylish.tst")));
         assertThat(Differ.generate(RES_FOLDER + "data.yaml", RES_FOLDER + "data2.yaml", "stylish")).
