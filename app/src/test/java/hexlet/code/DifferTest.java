13874600518d73815f431c1f81ebfccbbaa83cdb
John Snow <qaz20042004@gmail.com>
2024-07-24 13:30:35 +0300

    tmp json test removed

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -23,13 +23,13 @@ class DifferTest {
         assertThat(Differ.generate(RES_FOLDER + "data.yaml", RES_FOLDER + "data2.yaml", "plain")).
                 isEqualTo(Files.readString(Path.of(RES_FOLDER + "plain.tst")));
     }
-    @Test
+/*    @Test
     void jsonFormatTest() throws Exception {
         Differ.generate(RES_FOLDER + "data.java", RES_FOLDER + "data2.java", "json");
         var pathToJson = Path.of(System.getProperty("user.dir") + "/src/main/resources/output.json");
         assertThat(Files.exists(pathToJson));
         assertThat(Files.size(pathToJson) > 0);
         Files.deleteIfExists(pathToJson);
-    }
+    }*/
 
 }
