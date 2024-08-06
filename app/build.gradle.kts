467b893847ec94bf0e99046f009446578066283d
John Snow <qaz20042004@gmail.com>
2024-08-06 17:28:19 +0300

    build.gradle.kts substitute

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,55 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
+    application
+    jacoco
+    id ("checkstyle")
+    id ("com.adarshr.test-logger") version "3.0.0"
+    id ("com.github.ben-manes.versions") version "0.39.0"
+}
+application {
+    mainClass = "hexlet.code.App"
+}
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStackTraces = true
+        showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+    }
+}
+
+dependencies {
+    testImplementation("org.assertj:assertj-core:3.25.3")
+    testImplementation(platform("org.junit:junit-bom:5.11.0-M1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0-M1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation ("info.picocli:picocli:4.7.6")
+    implementation ("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.0-rc1")
+}
+
+
+
+/*plugins {
     application
     checkstyle
     jacoco
@@ -54,5 +105,4 @@ tasks.jacocoTestReport {
 
 application {
     mainClass = "hexlet.code.App"
-}
-
+}*/
