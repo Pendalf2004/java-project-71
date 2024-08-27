3a1d8413e03869312d35161e27d5979541819a06
John Snow <qaz20042004@gmail.com>
2024-08-27 11:42:55 +0300

    2nd check fixes. Fields removed. Tests reworked. Some additions in README.md. DataCompare made final.

--- app/src/main/java/hexlet/code/DataCompare.java
+++ app/src/main/java/hexlet/code/DataCompare.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-public class DataCompare {
+final public class DataCompare {
     public enum STATUS {
         UNCHANGED,
         REMOVED,
